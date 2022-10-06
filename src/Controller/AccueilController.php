bbd8f40b3f05a7cb226c6dbec53bb7a9bf4089be
melviinn <duchaufour.melvin.sio@gmail.com>
2022-10-06 17:46:42 +0200

    CSS Login

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccueilController extends AbstractController
 {
@@ -12,6 +13,13 @@ class AccueilController extends AbstractController
     public function index(): Response 
     {
         return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+    #[Route('/comptable', name: 'app_comptable')]  
+    public function comptable(): Response 
+    {
+        return $this->render('security/login.html.twig', [
             'controller_name' => 'AccueilController',
         ]);
     }
